6c269937ec529ab561a2d94f7481fdfb4286aa75
Anthony Diké <anthony.dke@gmail.com>
2019-06-07 11:48:40 -0400

    Two files demonstrating a Node and a LinkedList

---
+++ LinkedLists/congaLine.java
@@ -0,0 +1,11 @@
+public class congaLine {	
+	public class congaLineNode {
+		private String name;
+		private congaLineNode next;
+
+		public congaLineNode(String name, congaLineNode next) {
+			this.name = name;
+			this.next = next;
+		}
+	}
+}
