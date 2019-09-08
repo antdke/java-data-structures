a7d0c597ee0ac58eec238bc317d1de94c6d8fd1b
Anthony Diké <anthonydike@nyu.edu>
2019-09-08 17:47:31 -0400

    Added a push method to the Stack class

--- StacksAndQueues/stack.java
+++ StacksAndQueues/stack.java
@@ -7,4 +7,35 @@ public class Stack {
       this.data = data;
     }
   }
+
+  // with stacks, we add and remove from the end (the top of a stack of books -
+  // LIFO)
+  private Node top;
+
+  // check if stack has any nodes
+  public boolean isEmpty() {
+    return top == null;
+  }
+
+  // return the data in the top node
+  public int peek() {
+    if (top != null) {
+      return top.data;
+    }
+  }
+
+  // adds a node to the stack by pushing it on top of the stack (putting an extra
+  // book on top of the books)
+  public void push(int data) {
+    // create a node from the provided data
+    Node node = new Node(data);
+    // check if a top node exists before adding
+    if (top != null) {
+      // point the new node to the top of the stack
+      node.next = top;
+    }
+    // assign the new node as the top node
+    top = node;
+  }
+
 }
