ad6a54d6c1a9c54be41ebc35f9e1255a8d46d728
Anthony Diké <anthonydike@nyu.edu>
2019-09-08 16:34:45 -0400

    Deleted Extra LinkedList folder

--- LinkedLists/LinkedLists/congaLineNode.java
+++
@@ -1,9 +0,0 @@
-public class congaLineNode {
-	private String name;
-	private congaLineNode next;
-
-	public congaLineNode(String name, congaLineNode next) {
-		this.name = name;
-		this.next = next;
-	}
-}
